a4cc9035d55aa1e144a6a1a7389cb3bf0425fe01
Humberto Rocha <humrocha@gmail.com>
2018-11-28 19:14:16 -0300

    últimas alterações no exemplo jenkins com junit

--- src/com/home/junit/CalculatorTest.java
+++ src/com/home/junit/CalculatorTest.java
@@ -9,8 +9,8 @@ public class CalculatorTest  {
 	  @Test
 	  public void evaluatesExpression() {
 	    Calculator calculator = new Calculator();
-	    int sum = calculator.evaluate("1+2+3");
-	    assertEquals(6, sum);
+	    int sum = calculator.evaluate("2+2+3");
+	    assertEquals(7, sum);
 	  }
 	
 
